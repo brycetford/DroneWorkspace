cbaedd8509f636795edebab318f488b127bd5219
incarsaron <brycetford@gmail.com>
2023-10-12 23:51:30 -0400

    Working sitl

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
-# Basic entrypoint for ROS / Colcon Docker containers
 
-# Source ROS 2
-source /opt/ros/humble/setup.bash
-echo "Sourced ROS 2 Humble"
+# Start virtual X server in the background
+# - DISPLAY default is :99, set in dockerfile
+# - Users can override with `-e DISPLAY=` in `docker run` command to avoid
+#   running Xvfb and attach their screen
+if [[ -x "$(command -v Xvfb)" && "$DISPLAY" == ":99" ]]; then
+	echo "Starting Xvfb"
+	Xvfb :99 -screen 0 1600x1200x24+32 &
+fi
 
-exec "$@"
+# Check if the ROS_DISTRO is passed and use it
+# to source the ROS environment
+if [ -n "${ROS_DISTRO}" ]; then
+	source "/opt/ros/$ROS_DISTRO/setup.bash"
+fi
+
+# Use the LOCAL_USER_ID if passed in at runtime
+if [ -n "${LOCAL_USER_ID}" ]; then
+	echo "Starting with UID : $LOCAL_USER_ID"
+	# modify existing user's id
+	usermod -u $LOCAL_USER_ID user
+	# run as user
+	exec gosu user "$@"
+else
+	exec "$@"
+fi
