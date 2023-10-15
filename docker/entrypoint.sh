7bcac8adcfed79b2a033936fd6ce5e885b179912
incarsaron <Bryce>
2023-10-15 02:00:25 -0400

    fixed ros2 source in dev container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,16 +4,14 @@
 # - DISPLAY default is :99, set in dockerfile
 # - Users can override with `-e DISPLAY=` in `docker run` command to avoid
 #   running Xvfb and attach their screen
+
 if [[ -x "$(command -v Xvfb)" && "$DISPLAY" == ":99" ]]; then
 	echo "Starting Xvfb"
 	Xvfb :99 -screen 0 1600x1200x24+32 &
 fi
 
-# Check if the ROS_DISTRO is passed and use it
-# to source the ROS environment
-if [ -n "${ROS_DISTRO}" ]; then
-	source "/opt/ros/$ROS_DISTRO/setup.bash"
-fi
+source "/opt/ros/humble/setup.bash"
+echo "Sourced ROS 2 Humble"
 
 # Use the LOCAL_USER_ID if passed in at runtime
 if [ -n "${LOCAL_USER_ID}" ]; then
